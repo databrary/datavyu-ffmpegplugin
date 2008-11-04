082419ffd8eda4400e1ffa0889a75c5b67003257
Clinton <clinton.freeman@nicta.com.au>
2008-11-05 09:21:12 +1000

    Added initial sources

---
+++ src/main/java/au/com/nicta/openshapa/App.java
@@ -0,0 +1,13 @@
+package au.com.nicta.openshapa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
