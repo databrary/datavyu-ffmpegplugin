13b02a3456e5577110ad45c6b8737c721a80cceb
Clinton <clinton.freeman@nicta.com.au>
2008-11-05 12:05:56 +1000

    clintons change

--- src/main/java/au/com/nicta/openshapa/App.java
+++ src/main/java/au/com/nicta/openshapa/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hello World!");
-        System.out.println("Stephens Change!");
+        System.out.println("Clintons Change!");
     }
 }
