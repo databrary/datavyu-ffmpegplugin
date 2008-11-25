d70d06f669daf261a72cf4a550dcb01d53380996
Stephen <swhitcher@nicta.com.au>
2008-11-25 14:21:33 +1000

    Integrate spreadsheet with Database2

--- src/main/java/au/com/nicta/openshapa/disc/spreadsheet/Spreadsheet.java
+++ src/main/java/au/com/nicta/openshapa/disc/spreadsheet/Spreadsheet.java
@@ -9,6 +9,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.util.Vector;
 import javax.swing.JFrame;
+import org.apache.log4j.Logger;
 
 
 /**
@@ -17,7 +18,10 @@ import javax.swing.JFrame;
  * @author  FGA
  */
 public class Spreadsheet extends JFrame {
-  
+
+    /** Logger for this class. */
+    private static Logger logger = Logger.getLogger(Spreadsheet.class);
+
     /** Linear View orientation. */
     public static final int LINEAR_VIEW        = 1;
     /** Semi-temporal View orientation. */
@@ -47,8 +51,8 @@ public class Spreadsheet extends JFrame {
     /** Creates new form Spreadsheet. */
     public Spreadsheet() {
 
-    initComponents();
-  }
+        initComponents();
+    }
 
     /**
      * Creates new form Spreadsheet.
@@ -58,21 +62,23 @@ public class Spreadsheet extends JFrame {
      *
      * @throws SystemErrorException if the db does not create
      */
-  public Spreadsheet(Executive exec, Database db)
+    public Spreadsheet(Executive exec, Database db)
                                                 throws SystemErrorException {
-    this();
-    this.setExecutive(exec);
-    this.setDatabase(db);
+        this();
+        this.setExecutive(exec);
+        this.setDatabase(db);
 
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
         this.Populate();
         this.setSize(new Dimension(50,400));
-  }
+    }
 
+    /**
+     * Populate from the database.
+     */
     public void Populate() {
-        // Populate table with a variable listing from the database
         try {
             Vector<DataColumn> dbColumns = getDatabase().getDataColumns();
 
@@ -85,7 +91,9 @@ public class Spreadsheet extends JFrame {
                 col.setOffsetVisible(true);
                 col.setDataVisible(true);
 
-                for(int j=0; j < dbColumn.getNumCells(); j++) {
+                addColumn(col);
+
+                for(int j = 0; j < dbColumn.getNumCells(); j++) {
                     DataCell dc = (DataCell)getDatabase()
                                             .getCell(dbColumn.getID(), j);
 
@@ -96,109 +104,154 @@ public class Spreadsheet extends JFrame {
                 }
             }
         } catch (SystemErrorException e) {
-           // TODO: bug #18 Log the nature of the error to log4j.
+           logger.error("Failed to populate Spreadsheet.", e);
         }
 
     }
 
+    /**
+     * Set Executive
+     */
     public void setExecutive(Executive exec) {
-    this.executive = exec;
-  }
+        this.executive = exec;
+    }
 
+    /**
+     * Set Database
+     */
     public void setDatabase(Database db) {
-    this.database = db;
-  }
+        this.database = db;
+    }
 
+    /**
+     * @return Executive for this spreadsheet
+     */
     public Executive getExecutive() {
-    return (this.executive);
-  }
+        return (this.executive);
+    }
 
+    /**
+     * @return Database this spreadsheet displays
+     */
     public Database getDatabase() {
-    return (this.database);
-  }
+        return (this.database);
+    }
 
+    /**
+     * Add a column
+     * @param col Column to add
+     */
     public void addColumn(SpreadsheetColumn col) throws SystemErrorException {
-    this.columns.addElement(col);
-    this.add(col);
-    this.updateSpreadsheet();
-  }
-  
-  public void removeColumn(SpreadsheetColumn col)
+        this.columns.addElement(col);
+        this.add(col);
+        this.updateSpreadsheet();
+    }
+
+    /**
+     * Remove a column
+     * @param col Column to remove
+     */
+    public void removeColumn(SpreadsheetColumn col)
                                                 throws SystemErrorException {
-    this.columns.removeElement(col);
-    this.remove(col);
-    this.updateSpreadsheet();
-  }
+        this.columns.removeElement(col);
+        this.remove(col);
+        this.updateSpreadsheet();
+    }
 
+    /**
+     * @return Scale = 1 only for now
+     */
     public long getScale() {
-    return (1);
-  }
+        return (1);
+    }
 
+    /**
+     * @return Minimum Onset Timestamp found in database
+     */
     public long getMinTimeStamp() throws SystemErrorException {
-    if (!spreadsheetChanged) {
-      return (lastMinTimeStamp);
+        if (!spreadsheetChanged) {
+            return (lastMinTimeStamp);
+        }
+
+        long min = Long.MAX_VALUE;
+        long lm;
+
+        for (int i=0; i<this.columns.size(); i++) {
+            lm = columns.elementAt(i).getMinOnset();
+            if (lm < min) {
+            min = lm;
+            }
+        }
+
+        this.lastMinTimeStamp = min;
+        return (min);
     }
 
-    long min = Long.MAX_VALUE;
-    long lm;
-
-    for (int i=0; i<this.columns.size(); i++) {
-      lm = columns.elementAt(i).getMinOnset();
-      if (lm < min) {
-        min = lm;
-      }
-    }
-
-    this.lastMinTimeStamp = min;
-    return (min);
-  }
-
+    /**
+     * @return Maximum Onset Timestamp found in database
+     * @throws SystemErrorException TODO: should remove this throw
+     */
     public long getMaxTimeStamp() throws SystemErrorException {
-    if (!spreadsheetChanged) {
-      return (lastMaxTimeStamp);
+        if (!spreadsheetChanged) {
+            return (lastMaxTimeStamp);
+        }
+
+        long max = Long.MIN_VALUE;
+        long lm;
+
+        // TODO: MaxOnset doesn't seem enough - what about Offset?
+        for (int i = 0; i < this.columns.size(); i++) {
+            lm = columns.elementAt(i).getMaxOnset();
+            if (lm > max) {
+                max = lm;
+            }
+        }
+
+        this.lastMaxTimeStamp = max;
+        return (max);
     }
 
-    long max = Long.MIN_VALUE;
-    long lm;
-
-    for (int i=0; i<this.columns.size(); i++) {
-      lm = columns.elementAt(i).getMinOnset();
-      if (lm > max) {
-        max = lm;
-      }
-    }
-
-    this.lastMaxTimeStamp = max;
-    return (max);
-  }
-
+    /**
+     * @return Maximum Onset Timestamp found in database
+     * @throws SystemErrorException TODO: should remove this throw
+     */
     public int getColumnHeight() throws SystemErrorException {
-    long diff = (this.getMaxTimeStamp()-this.getMinTimeStamp())/this.getScale();
-    if (diff >= Integer.MAX_VALUE) {
-      return (Integer.MAX_VALUE-1);
+        long diff = (this.getMaxTimeStamp() - this.getMinTimeStamp())
+                                                            / this.getScale();
+        if (diff >= Integer.MAX_VALUE) {
+            return (Integer.MAX_VALUE-1);
+        }
+        return ((int)diff);
     }
 
-    return ((int)diff);
-  }
-
+    /**
+     * Recalculate Spreadsheet extents
+     * @throws SystemErrorException TODO: should remove this throw
+     */
     public void updateSpreadsheet() throws SystemErrorException {
-    this.spreadsheetChanged = true;
-    this.getMinTimeStamp();
-    this.getMaxTimeStamp();
-    this.spreadsheetChanged = false;
+        this.spreadsheetChanged = true;
+        this.getMinTimeStamp();
+        this.getMaxTimeStamp();
+        this.spreadsheetChanged = false;
         Populate();
-    this.repaint();
-  }
-
-    public int getSpreadsheetView() {
-    return (this.spreadsheetView);
-  }
-
-    public void setSpreadsheetView(int view) {
-        if ((view >= 1) && (view <= 3)) {
-      this.spreadsheetView = view;
+        this.repaint();
     }
-  }
+
+    /**
+     * @return Spreadsheet view orientation
+     */
+    public int getSpreadsheetView() {
+        return (this.spreadsheetView);
+    }
+
+    /**
+     * Set Spreadsheet view orientation
+     */
+    public void setSpreadsheetView(int view) {
+        if ((view >= LINEAR_VIEW) && (view <= TEMPORAL_VIEW)) {
+            this.spreadsheetView = view;
+        }
+    }
 
   /** This method is called from within the constructor to
    * initialize the form.
@@ -220,9 +273,9 @@ public class Spreadsheet extends JFrame {
       .add(0, 300, Short.MAX_VALUE)
     );
   }// </editor-fold>//GEN-END:initComponents
-  
-  
+
+
   // Variables declaration - do not modify//GEN-BEGIN:variables
   // End of variables declaration//GEN-END:variables
-  
+
 }
