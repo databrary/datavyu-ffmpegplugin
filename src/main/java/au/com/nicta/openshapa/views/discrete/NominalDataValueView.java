2b7864f2a94ae0156c62aa6dfff82f86fb32e8b4
Clinton <clinton.freeman@nicta.com.au>
2009-03-11 09:30:57 +1000

    BugzID:89 - Implemented the copy and paste functionality for the nominal editor.

--- src/main/java/au/com/nicta/openshapa/views/discrete/NominalDataValueView.java
+++ src/main/java/au/com/nicta/openshapa/views/discrete/NominalDataValueView.java
@@ -1,10 +1,17 @@
 package au.com.nicta.openshapa.views.discrete;
 
+import au.com.nicta.openshapa.OpenSHAPA;
 import au.com.nicta.openshapa.db.DataCell;
 import au.com.nicta.openshapa.db.Matrix;
 import au.com.nicta.openshapa.db.NominalDataValue;
 import au.com.nicta.openshapa.db.SystemErrorException;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
 import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Set;
 import org.apache.log4j.Logger;
 
 /**
@@ -17,6 +24,13 @@ public final class NominalDataValueView extends DataValueView {
 
     /** The logger for NominalDataValueView. */
     private static Logger logger = Logger.getLogger(NominalDataValueView.class);
+
+    /** Reserved characters are not permitted in a nominal value at all. */
+    private HashMap<Character, Character> reservedChars;
+
+    /** The reserved replacement is a character that replaces reserved
+     *  characters pasted into nominal views. */
+    private static final Character RESERVED_REPLACEMENT = '_';
 
     /**
      * Constructor.
@@ -37,6 +51,136 @@ public final class NominalDataValueView extends DataValueView {
                          final int matrixIndex,
                          final boolean editable) {
         super(cellSelection, cell, matrix, matrixIndex, editable);
+        reservedChars = new HashMap<Character, Character>();
+        reservedChars.put(')', ')');
+        reservedChars.put('(', '(');
+        reservedChars.put('<', '<');
+        reservedChars.put('>', '>');
+        reservedChars.put('|', '|');
+        reservedChars.put(',', ',');
+        reservedChars.put(';', ';');
+    }
+
+        /**
+     * The action to invoke when a key is pressed.
+     *
+     * @param e The key event that triggered this action.
+     */
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_BACK_SPACE:
+            case KeyEvent.VK_DELETE:
+                // Ignore - handled when the key is typed.
+                e.consume();
+                break;
+
+            case KeyEvent.VK_LEFT:
+                // If the character two steps to the left is a preserved
+                // character we need to skip one before passing the key event
+                // down to skip again (effectively skipping the preserved
+                // character).
+                for (int i = 0; i < getPreservedChars().size(); i++) {
+                    int c = Math.max(0, getCaretPosition() - 2);
+
+                    if (getText().charAt(c) == getPreservedChars().get(i)) {
+                        setCaretPosition(Math.max(0, getCaretPosition() - 1));
+                        break;
+                    }
+                }
+                break;
+
+            case KeyEvent.VK_RIGHT:
+                // If the character to the right is a preserved character, we
+                // need to skip one before passing the key event down to skip
+                // again (effectively skipping the preserved character).
+                for (int i = 0; i < getPreservedChars().size(); i++) {
+                    int c = Math.min(getText().length() - 1,
+                                     getCaretPosition() + 1);
+                    if (getText().charAt(c) == getPreservedChars().get(i)) {
+                        setCaretPosition(Math.min(getText().length() - 1,
+                                                  getCaretPosition() + 1));
+                        break;
+                    }
+                }
+                break;
+
+            case KeyEvent.VK_V:
+                // Depending on platform, check appropriate modifier keys and
+                // paste into timestamp.
+                switch (OpenSHAPA.getPlatform()) {
+                    case MAC:
+                        if (e.isMetaDown()) {
+                            pasteNominal(e);
+                        }
+                        break;
+                    default:
+                        if (e.isControlDown()) {
+                            pasteNominal(e);
+                        }
+                        break;
+                }
+                break;
+
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_UP:
+                // Key stroke gets passed up a parent element to navigate
+                // cells up and down.
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Pastes contents of the clipboard into the nominal data value view.
+     *
+     * @param e The key event that trigged this paste nominal action.
+     */
+    public void pasteNominal(KeyEvent e) {
+        // Consume the paste event.
+        e.consume();
+
+        // Get the contents of the clipboard.
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        Transferable contents = clipboard.getContents(null);
+        boolean hasText = (contents != null)
+                     && contents.isDataFlavorSupported(DataFlavor.stringFlavor);
+
+        // No valid text in clipboard. Bail.
+        if (!hasText) {
+            return;
+        }
+
+        // Valid text in clipboard - attempt to copy it into timestamp.
+        try {
+            String text = (String) contents
+                                   .getTransferData(DataFlavor.stringFlavor);
+
+            // Replace reserved characters with a suitable replacement.
+            Set<Character> reservedSet = reservedChars.keySet();
+            for (Character reservedChar : reservedSet) {
+                text = text.replace(reservedChar, RESERVED_REPLACEMENT);
+            }
+
+            // Update the text field and set the new caret position.
+            StringBuffer fieldContents = new StringBuffer(this.getText());
+            fieldContents.insert(getCaretPosition(), text);
+            storeCaretPosition();
+            setText(fieldContents.toString());
+            restoreCaretPosition();
+            setCaretPosition(getCaretPosition() + text.length());
+
+            // Push the character changes into the database.
+            NominalDataValue ndv = (NominalDataValue) getValue();
+            ndv.setItsValue(fieldContents.toString());
+            updateDatabase();
+
+        } catch (SystemErrorException se) {
+            logger.error("Unable to edit text string", se);
+        } catch (Exception ex) {
+            logger.error("Unable to get clipboard contents", ex);
+        }
     }
 
     /**
@@ -61,10 +205,7 @@ public final class NominalDataValueView extends DataValueView {
 
         // Just a regular vanilla keystroke - insert it into text field.
         } else if (!e.isMetaDown() && !e.isControlDown()
-                   && (e.getKeyChar() != ')' && e.getKeyChar() != '('
-                       && e.getKeyChar() != '<' && e.getKeyChar() != '>'
-                       && e.getKeyChar() != '|' && e.getKeyChar() != ','
-                       && e.getKeyChar() != ';')) {
+                   && !reservedChars.containsKey(e.getKeyChar())) {
             this.removeSelectedText();
             StringBuffer currentValue = new StringBuffer(getText());
             currentValue.insert(getCaretPosition(), e.getKeyChar());
