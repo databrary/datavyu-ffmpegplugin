b9e4b8f4695913876f83e5a223a162d785ad8e19
Clinton <clinton.freeman@nicta.com.au>
2009-03-23 09:36:14 +1000

    Added comments for methods in HashUtils

--- src/main/java/au/com/nicta/openshapa/util/HashUtils.java
+++ src/main/java/au/com/nicta/openshapa/util/HashUtils.java
@@ -6,6 +6,14 @@ package au.com.nicta.openshapa.util;
  * @author cfreeman
  */
 public final class HashUtils {
+
+    /**
+     * Generates a hash code for the supplied object.
+     *
+     * @param obj The object to generate a hash code for.
+     *
+     * @return The hashcode for the object, 0 if the supplied object is null.
+     */
     public static int Obj2H(final Object obj) {
         if (obj == null) {
             return 0;
@@ -14,6 +22,13 @@ public final class HashUtils {
         }
     }
 
+    /**
+     * Generates an integer hashcode from a long value.
+     *
+     * @param l The long value to turn into an integer hashCode.
+     *
+     * @return The integer hashcode for the long value.
+     */
     public static int Long2H(final long l) {
         return (int)(l ^ (l >>> 32));
     }
