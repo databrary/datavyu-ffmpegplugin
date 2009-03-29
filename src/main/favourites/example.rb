2c9e41dda7b9c74fef030e353ea01578de66504d
Clinton <clinton.freeman@nicta.com.au>
2009-03-30 09:48:47 +1000

    BugsID:133 - Added example script to the favourites directory.

---
+++ src/main/favourites/example.rb
@@ -0,0 +1,8 @@
+
+puts "Welcome to OpenSHAPA, we have great scripting features that
+allow you to manipulate the database with ruby scripts. This is
+an example of a favourite script. Simply put your favourite scripts
+in the 'favourites' folder of your OpenSHAPA installation and they
+will automatically turn up in this list. For more information on
+scripting and what it can do for you visit the OpenSHAPA website
+for more information."
