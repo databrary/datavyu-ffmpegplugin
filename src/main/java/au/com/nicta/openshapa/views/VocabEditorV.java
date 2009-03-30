d408c61dd7fc8739de53073247ada619ccb547aa
Clinton <clinton.freeman@nicta.com.au>
2009-03-30 14:40:14 +1000

    BugzID:274 - Added tool tips to many of the components.

--- src/main/java/au/com/nicta/openshapa/views/VocabEditorV.java
+++ src/main/java/au/com/nicta/openshapa/views/VocabEditorV.java
@@ -1,7 +1,16 @@
 package au.com.nicta.openshapa.views;
 
+import au.com.nicta.openshapa.OpenSHAPA;
+import au.com.nicta.openshapa.db.Database;
+import au.com.nicta.openshapa.db.MatrixVocabElement;
+import au.com.nicta.openshapa.db.PredicateVocabElement;
+import au.com.nicta.openshapa.db.SystemErrorException;
 import java.awt.event.ActionListener;
+import javax.swing.table.DefaultTableModel;
+import org.apache.log4j.Logger;
 import org.jdesktop.application.Action;
+import org.jdesktop.application.Application;
+import org.jdesktop.application.ResourceMap;
 
 /**
  *
@@ -9,13 +18,101 @@ import org.jdesktop.application.Action;
  */
 public class VocabEditorV extends OpenSHAPADialog {
 
+    private DefaultTableModel tableModel;
+
+    private Database db;
+
+    /** The logger for OpenSHAPA. */
+    private static Logger logger = Logger.getLogger(VocabEditorV.class);
+
+    /** The ID of the column that holds delta information. */
+    private static final int DELTA_COLUMN_ID = 0;
+
+    /** The ID of the column that holds type information. */
+    private static final int TYPE_COLUMN_ID = 1;
+
+    /** The ID of the column that holds the actual vocab element. */
+    private static final int VOCAB_COLUMN_ID = 2;
+
+    /** The number of columns in the table model. */
+    private static final int NUM_COLUMNS = 3;
+
+    private boolean containsChange;
+
     /** Creates new form VocabEditorV */
     public VocabEditorV(java.awt.Frame parent, boolean modal,
                         final ActionListener listener) {
         super(parent, modal);
+
+        db = OpenSHAPA.getDatabase();
+
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("Delta");
+        tableModel.addColumn("Type");
+        tableModel.addColumn("vocab");
+
+        containsChange = false;
+
         initComponents();
     }
 
+    /**
+     * 
+     */
+    @Action
+    public void addPredicate() {
+        Object row[] = new Object[NUM_COLUMNS];
+        
+        row[DELTA_COLUMN_ID] = new String(".");
+        row[TYPE_COLUMN_ID] = new String("P");
+        try {
+            row[VOCAB_COLUMN_ID] = new PredicateVocabElement(db, "predicate");
+        } catch (SystemErrorException e) {
+            logger.error("Unable to create predicate vocab element", e);
+        }
+
+        tableModel.addRow(row);
+        containsChange = true;
+        this.updateCloseButton();
+    }
+
+    /**
+     *
+     */
+    @Action
+    public void addMatrix() {
+        Object row[] = new Object[NUM_COLUMNS];
+
+        row[DELTA_COLUMN_ID] = new String(".");
+        row[TYPE_COLUMN_ID] = new String("M");
+        try {
+            row[VOCAB_COLUMN_ID] = new MatrixVocabElement(db, "matrix");
+        } catch (SystemErrorException e) {
+            logger.error("Unable to create matrix vocab element", e);
+        }
+
+        tableModel.addRow(row);
+        containsChange = true;
+        this.updateCloseButton();
+    }
+
+    private void updateCloseButton() {
+        ResourceMap rMap = Application.getInstance(OpenSHAPA.class)
+                                      .getContext()
+                                      .getResourceMap(VocabEditorV.class);
+
+        if (containsChange) {
+            closeButton.setText(rMap.getString("closeButton.cancelText"));
+            closeButton.setToolTipText(rMap.getString("closeButton.cancelTip"));
+        } else {
+            closeButton.setText(rMap.getString("closeButton.closeText"));
+            closeButton.setToolTipText(rMap.getString("closeButton.closeTip"));
+        }
+    }
+
+    /**
+     * 
+     */
     @Action
     public void closeWindow() {
         this.setVisible(false);
@@ -35,16 +132,16 @@ public class VocabEditorV extends OpenSHAPADialog {
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jComboBox1 = new javax.swing.JComboBox();
-        jCheckBox1 = new javax.swing.JCheckBox();
-        jButton6 = new javax.swing.JButton();
+        addArgButton = new javax.swing.JButton();
+        argTypeComboBox = new javax.swing.JComboBox();
+        varyArgCheckBox = new javax.swing.JCheckBox();
+        deleteButton = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jButton7 = new javax.swing.JButton();
-        jButton8 = new javax.swing.JButton();
-        jButton9 = new javax.swing.JButton();
-        jButton10 = new javax.swing.JButton();
+        revertButton = new javax.swing.JButton();
+        applyButton = new javax.swing.JButton();
+        okButton = new javax.swing.JButton();
+        closeButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("au/com/nicta/openshapa/views/resources/VocabEditorV"); // NOI18N
@@ -53,67 +150,74 @@ public class VocabEditorV extends OpenSHAPADialog {
         setResizable(false);
         getContentPane().setLayout(new java.awt.GridBagLayout());
 
-        jButton1.setText(bundle.getString("jButton.text")); // NOI18N
+        javax.swing.ActionMap actionMap = org.jdesktop.application.Application.getInstance(au.com.nicta.openshapa.OpenSHAPA.class).getContext().getActionMap(VocabEditorV.class, this);
+        jButton1.setAction(actionMap.get("addPredicate")); // NOI18N
+        jButton1.setText(bundle.getString("addPredicateButton.text")); // NOI18N
+        jButton1.setToolTipText(bundle.getString("addPredicateButton.tip")); // NOI18N
         jButton1.setName("jButton1"); // NOI18N
         getContentPane().add(jButton1, new java.awt.GridBagConstraints());
 
-        jButton2.setText(bundle.getString("jButton2.text")); // NOI18N
+        jButton2.setAction(actionMap.get("addMatrix")); // NOI18N
+        jButton2.setText(bundle.getString("addMatrixButton.text")); // NOI18N
+        jButton2.setToolTipText(bundle.getString("addMatrixButton.tip")); // NOI18N
         jButton2.setName("jButton2"); // NOI18N
         getContentPane().add(jButton2, new java.awt.GridBagConstraints());
 
-        jButton3.setText(bundle.getString("jButton3.text")); // NOI18N
+        jButton3.setText(bundle.getString("moveArgLeftButton.text")); // NOI18N
+        jButton3.setToolTipText(bundle.getString("moveArgLeftButton.tip")); // NOI18N
+        jButton3.setEnabled(false);
         jButton3.setName("jButton3"); // NOI18N
         getContentPane().add(jButton3, new java.awt.GridBagConstraints());
 
-        jButton4.setText(bundle.getString("jButton4.text")); // NOI18N
+        jButton4.setText(bundle.getString("moveArgRightButton.text")); // NOI18N
+        jButton4.setToolTipText(bundle.getString("moveArgRightButton.tip")); // NOI18N
+        jButton4.setEnabled(false);
         jButton4.setName("jButton4"); // NOI18N
         getContentPane().add(jButton4, new java.awt.GridBagConstraints());
 
-        jButton5.setText(bundle.getString("jButton5.text")); // NOI18N
-        jButton5.setName("jButton5"); // NOI18N
+        addArgButton.setText(bundle.getString("addArgButton.text")); // NOI18N
+        addArgButton.setToolTipText(bundle.getString("addArgButton.tip")); // NOI18N
+        addArgButton.setEnabled(false);
+        addArgButton.setName("addArgButton"); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        getContentPane().add(jButton5, gridBagConstraints);
+        getContentPane().add(addArgButton, gridBagConstraints);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jComboBox1.setName("jComboBox1"); // NOI18N
+        argTypeComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        argTypeComboBox.setToolTipText(bundle.getString("argTypeComboBox.tip")); // NOI18N
+        argTypeComboBox.setEnabled(false);
+        argTypeComboBox.setName("argTypeComboBox"); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        getContentPane().add(jComboBox1, gridBagConstraints);
+        getContentPane().add(argTypeComboBox, gridBagConstraints);
 
-        jCheckBox1.setText(bundle.getString("jCheckBox1.text")); // NOI18N
-        jCheckBox1.setName("jCheckBox1"); // NOI18N
+        varyArgCheckBox.setText(bundle.getString("varyArgCheckBox.text")); // NOI18N
+        varyArgCheckBox.setToolTipText(bundle.getString("varyArgCheckBox.tip")); // NOI18N
+        varyArgCheckBox.setEnabled(false);
+        varyArgCheckBox.setName("varyArgCheckBox"); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        getContentPane().add(jCheckBox1, gridBagConstraints);
+        getContentPane().add(varyArgCheckBox, gridBagConstraints);
 
-        jButton6.setText(bundle.getString("jButton6.text")); // NOI18N
-        jButton6.setName("jButton6"); // NOI18N
+        deleteButton.setText(bundle.getString("deleteButton.text")); // NOI18N
+        deleteButton.setToolTipText(bundle.getString("deleteButton.tip")); // NOI18N
+        deleteButton.setEnabled(false);
+        deleteButton.setName("deleteButton"); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 3;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        getContentPane().add(jButton6, gridBagConstraints);
+        getContentPane().add(deleteButton, gridBagConstraints);
 
         jScrollPane1.setName("jScrollPane1"); // NOI18N
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
+        jTable1.setModel(tableModel);
         jTable1.setName("jTable1"); // NOI18N
         jScrollPane1.setViewportView(jTable1);
 
@@ -126,58 +230,64 @@ public class VocabEditorV extends OpenSHAPADialog {
         gridBagConstraints.weighty = 1.0;
         getContentPane().add(jScrollPane1, gridBagConstraints);
 
-        jButton7.setText(bundle.getString("jButton7.text")); // NOI18N
-        jButton7.setName("jButton7"); // NOI18N
+        revertButton.setText(bundle.getString("revertButton.text")); // NOI18N
+        revertButton.setToolTipText(bundle.getString("revertButton.tip")); // NOI18N
+        revertButton.setEnabled(false);
+        revertButton.setName("revertButton"); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 3;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
-        getContentPane().add(jButton7, gridBagConstraints);
+        getContentPane().add(revertButton, gridBagConstraints);
 
-        jButton8.setText(bundle.getString("jButton8.text")); // NOI18N
-        jButton8.setName("jButton8"); // NOI18N
+        applyButton.setText(bundle.getString("applyButton.text")); // NOI18N
+        applyButton.setToolTipText(bundle.getString("applyButton.tip")); // NOI18N
+        applyButton.setEnabled(false);
+        applyButton.setName("applyButton"); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 3;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        getContentPane().add(jButton8, gridBagConstraints);
+        getContentPane().add(applyButton, gridBagConstraints);
 
-        jButton9.setText(bundle.getString("jButton9.text")); // NOI18N
-        jButton9.setName("jButton9"); // NOI18N
+        okButton.setText(bundle.getString("okButton.text")); // NOI18N
+        okButton.setToolTipText(bundle.getString("okButton.tip")); // NOI18N
+        okButton.setEnabled(false);
+        okButton.setName("okButton"); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 3;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
-        getContentPane().add(jButton9, gridBagConstraints);
+        getContentPane().add(okButton, gridBagConstraints);
 
-        javax.swing.ActionMap actionMap = org.jdesktop.application.Application.getInstance(au.com.nicta.openshapa.OpenSHAPA.class).getContext().getActionMap(VocabEditorV.class, this);
-        jButton10.setAction(actionMap.get("closeWindow")); // NOI18N
-        jButton10.setText(bundle.getString("jButton10.text")); // NOI18N
-        jButton10.setName("jButton10"); // NOI18N
+        closeButton.setAction(actionMap.get("closeWindow")); // NOI18N
+        closeButton.setText(bundle.getString("closeButton.closeText")); // NOI18N
+        closeButton.setToolTipText(bundle.getString("closeButton.closeTip")); // NOI18N
+        closeButton.setName("closeButton"); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 3;
         gridBagConstraints.gridy = 3;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        getContentPane().add(jButton10, gridBagConstraints);
+        getContentPane().add(closeButton, gridBagConstraints);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton addArgButton;
+    private javax.swing.JButton applyButton;
+    private javax.swing.JComboBox argTypeComboBox;
+    private javax.swing.JButton closeButton;
+    private javax.swing.JButton deleteButton;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
-    private javax.swing.JButton jButton8;
-    private javax.swing.JButton jButton9;
-    private javax.swing.JCheckBox jCheckBox1;
-    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JButton okButton;
+    private javax.swing.JButton revertButton;
+    private javax.swing.JCheckBox varyArgCheckBox;
     // End of variables declaration//GEN-END:variables
 
 }
