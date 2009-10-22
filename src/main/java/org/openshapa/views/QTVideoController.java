1d1f7993ce11459cf07059438be54b22c4ff7e26
Clinton <clinton.freeman@nicta.com.au>
2009-10-22 13:49:57 +1000

    BugzID:345 - Current playback speed displayed in data controller.

--- src/main/java/org/openshapa/views/QTVideoController.java
+++ src/main/java/org/openshapa/views/QTVideoController.java
@@ -3,7 +3,6 @@ package org.openshapa.views;
 import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -15,6 +14,7 @@ import org.apache.log4j.Logger;
 import org.jdesktop.application.Action;
 import org.openshapa.db.TimeStamp;
 import org.openshapa.OpenSHAPA;
+import org.openshapa.util.FloatUtils;
 import org.openshapa.views.continuous.DataViewer;
 import org.openshapa.views.continuous.PluginManager;
 
@@ -105,6 +105,12 @@ public final class QTVideoController extends OpenSHAPADialog {
         initComponents();
         setName(this.getClass().getSimpleName());
         viewers = new HashSet<DataViewer>();
+
+        // Hide unsupported features.
+        this.syncVideoButton.setEnabled(false);
+        this.syncButton.setEnabled(false);
+        this.syncCtrlButton.setEnabled(false);
+        this.timestampSetupButton.setEnabled(false);
     }
 
     //--------------------------------------------------------------------------
@@ -197,7 +203,6 @@ public final class QTVideoController extends OpenSHAPADialog {
         setNewCellOnsetButton = new javax.swing.JButton();
         fillerPanel = new javax.swing.JPanel();
         timestampSetupButton = new javax.swing.JButton();
-        videoProgressBar = new javax.swing.JSlider();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Quicktime Video Controller");
@@ -224,9 +229,8 @@ public final class QTVideoController extends OpenSHAPADialog {
         });
         topPanel.add(openVideoButton, java.awt.BorderLayout.LINE_START);
 
-        lblSpeed.setFont(new java.awt.Font("Courier New", 1, 12)); // NOI18N
-        lblSpeed.setText("*1.0");
-        lblSpeed.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        lblSpeed.setText("0");
+        lblSpeed.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 2));
         topPanel.add(lblSpeed, java.awt.BorderLayout.LINE_END);
 
         mainPanel.add(topPanel, java.awt.BorderLayout.NORTH);
@@ -236,14 +240,12 @@ public final class QTVideoController extends OpenSHAPADialog {
 
         javax.swing.ActionMap actionMap = org.jdesktop.application.Application.getInstance(org.openshapa.OpenSHAPA.class).getContext().getActionMap(QTVideoController.class, this);
         syncCtrlButton.setAction(actionMap.get("syncCtrlAction")); // NOI18N
-        syncCtrlButton.setIcon(resourceMap.getIcon("syncCtrlButton.icon")); // NOI18N
         syncCtrlButton.setMaximumSize(new java.awt.Dimension(32, 32));
         syncCtrlButton.setMinimumSize(new java.awt.Dimension(32, 32));
         syncCtrlButton.setPreferredSize(new java.awt.Dimension(32, 32));
         gridButtonPanel.add(syncCtrlButton);
 
         syncButton.setAction(actionMap.get("syncAction")); // NOI18N
-        syncButton.setIcon(resourceMap.getIcon("syncButton.icon")); // NOI18N
         gridButtonPanel.add(syncButton);
 
         setCellOnsetButton.setAction(actionMap.get("setCellOnsetAction")); // NOI18N
@@ -302,13 +304,6 @@ public final class QTVideoController extends OpenSHAPADialog {
 
         rightTimePanel.setBackground(java.awt.Color.white);
         rightTimePanel.setLayout(new java.awt.GridLayout(4, 1));
-
-        syncVideoButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/QTVideoController/eng/syncVideoButton.png"))); // NOI18N
-        syncVideoButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                syncVideoButtonActionPerformed(evt);
-            }
-        });
         rightTimePanel.add(syncVideoButton);
 
         goBackTextField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -339,24 +334,9 @@ public final class QTVideoController extends OpenSHAPADialog {
 
         fillerPanel.setBackground(java.awt.Color.white);
         fillerPanel.setLayout(new java.awt.BorderLayout());
-
-        timestampSetupButton.setIcon(resourceMap.getIcon("timestampSetupButton.icon")); // NOI18N
-        timestampSetupButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                timestampSetupButtonActionPerformed(evt);
-            }
-        });
         fillerPanel.add(timestampSetupButton, java.awt.BorderLayout.CENTER);
 
         bottomPanel.add(fillerPanel, java.awt.BorderLayout.EAST);
-
-        videoProgressBar.setBackground(java.awt.Color.white);
-        videoProgressBar.addChangeListener(new javax.swing.event.ChangeListener() {
-            public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                videoProgressBarStateChanged(evt);
-            }
-        });
-        bottomPanel.add(videoProgressBar, java.awt.BorderLayout.SOUTH);
 
         mainPanel.add(bottomPanel, java.awt.BorderLayout.SOUTH);
 
@@ -485,13 +465,13 @@ public final class QTVideoController extends OpenSHAPADialog {
      */
     @Action
     public void stopAction() {
-        lblSpeed.setText(String.format("x%(.3f", PLAY_RATE));
+        lblSpeed.setText(FloatUtils.doubleToFractionStr(new Double(PLAY_RATE)));
         for (DataViewer viewer : viewers) {
             viewer.stop();
             viewer.setPlaybackSpeed(PLAY_RATE);
         }
         shuttleDirection = ShuttleDirection.UNDEFINED;
-        isPaused = true;      // @todo: handle stop->pause btn combination?
+        isPaused = true;
     }
 
     /**
@@ -557,7 +537,7 @@ public final class QTVideoController extends OpenSHAPADialog {
 
             for (DataViewer viewer : viewers) {
                 viewer.seek(milli);
-                // viewer.play();   // @todo Leave the viewer in its current play state
+                // viewer.play();
             }
 
         } catch (ParseException e) {
@@ -626,7 +606,7 @@ public final class QTVideoController extends OpenSHAPADialog {
      */
     private void shuttleAt(final float rate) {
         isPaused = false;
-        lblSpeed.setText(String.format("x%(.3f", rate));
+        lblSpeed.setText(FloatUtils.doubleToFractionStr(new Double(rate)));
         for (DataViewer viewer : viewers) {
             viewer.setPlaybackSpeed(rate);
             viewer.play();
@@ -638,7 +618,7 @@ public final class QTVideoController extends OpenSHAPADialog {
      * @param step Milliseconds to jump.
      */
     private void jump(final long step) {
-        lblSpeed.setText(String.format("x%(.3f", PLAY_RATE));
+        lblSpeed.setText(FloatUtils.doubleToFractionStr(new Double(PLAY_RATE)));
         for (DataViewer viewer : viewers) {
             viewer.stop();
             viewer.setPlaybackSpeed(PLAY_RATE);
@@ -678,27 +658,16 @@ public final class QTVideoController extends OpenSHAPADialog {
     public void syncVideoAction() {
     }
 
-    private void syncVideoButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_syncVideoButtonActionPerformed
-    }//GEN-LAST:event_syncVideoButtonActionPerformed
-
     /**
      * Action to invoke when the user clicks on the time stamp setup button.
      *
      * @param evt The event that triggered this action.
      */
-    private void timestampSetupButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_timestampSetupButtonActionPerformed
-    }//GEN-LAST:event_timestampSetupButtonActionPerformed
-
     /**
      * Action to invoke when the video progress bar state changes.
      *
      * @param evt The event that triggered this action.
      */
-    private void videoProgressBarStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_videoProgressBarStateChanged
-        if (!this.videoProgressBar.getValueIsAdjusting()) {
-        }
-    }//GEN-LAST:event_videoProgressBarStateChanged
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel bottomPanel;
     private javax.swing.JButton createNewCellButton;
@@ -731,7 +700,6 @@ public final class QTVideoController extends OpenSHAPADialog {
     private javax.swing.JLabel timestampLabel;
     private javax.swing.JButton timestampSetupButton;
     private javax.swing.JPanel topPanel;
-    private javax.swing.JSlider videoProgressBar;
     // End of variables declaration//GEN-END:variables
 
     /** Logger for this class. */
