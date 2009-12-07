671273c67991a1236cd7181f932973a5e39adb9e
Douglas Teoh <douglas.teoh@nicta.com.au>
2009-12-07 16:44:02 +1000

    BugzID:852 - Using .shapa file extension.

--- src/main/java/org/openshapa/controllers/SaveProjectC.java
+++ src/main/java/org/openshapa/controllers/SaveProjectC.java
@@ -1,10 +1,8 @@
 package org.openshapa.controllers;
 
-import java.beans.IntrospectionException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Set;
 import org.apache.log4j.Logger;
 import org.openshapa.OpenSHAPA;
 import org.openshapa.project.Project;
@@ -12,8 +10,6 @@ import org.openshapa.project.OpenSHAPAProjectRepresenter;
 import org.yaml.snakeyaml.Dumper;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.introspector.Property;
-import org.yaml.snakeyaml.representer.Representer;
 
 /**
  * Controller for saving the OpenSHAPA project to disk.
@@ -28,11 +24,10 @@ public class SaveProjectC {
 
         Dumper dumper = new Dumper(new OpenSHAPAProjectRepresenter(), new DumperOptions());
         Yaml yaml = new Yaml(dumper);
-//        Yaml yaml = new Yaml();
 
         String fileName = outFile;
-        if (! fileName.endsWith(".openshapa")) {
-            fileName = fileName.concat(".openshapa");
+        if (! fileName.endsWith(".shapa")) {
+            fileName = fileName.concat(".shapa");
         }
 
         try {
@@ -43,27 +38,5 @@ public class SaveProjectC {
             logger.error("Unable to save project file", ex);
         }
     }
-
-//    private class SHAPARepresenter extends Representer {
-//        @Override
-//        protected Set<Property> getProperties(Class<? extends Object> type)
-//                throws IntrospectionException {
-//            Set<Property> set = super.getProperties(type);
-//            if (type.equals(Project.class)) {
-//                for (Property prop : set) {
-//                    if (prop.getName().equals("mediaViewerSettings")) {
-//                        set.remove(prop);
-//                    }
-//                    if (prop.getName().equals("mediaFiles")) {
-//                        set.remove(prop);
-//                    }
-//                    if (prop.getName().equals("changed")) {
-//                        set.remove(prop);
-//                    }
-//                }
-//            }
-//            return set;
-//        }
-//    }
 
 }
