0e7719833da06d4fd0c24658c03779374f3a304c
Michael Muthukrishna <michael.muthukrishna@nicta.com.au>
2009-12-09 13:48:07 +1000

    BugzID:742 - Open dialog remembers previous location. Also added Bug583test that needs to be uncommented

---
+++ src/test/java/org/openshapa/uitests/UIBug583Test.java
@@ -0,0 +1,90 @@
+package org.openshapa.uitests;
+
+import java.io.File;
+import org.uispec4j.interception.MainClassAdapter;
+import org.uispec4j.interception.WindowInterceptor;
+import org.openshapa.OpenSHAPA;
+import org.openshapa.views.discrete.SpreadsheetPanel;
+import java.util.Vector;
+import org.uispec4j.Cell;
+import org.uispec4j.Key;
+import org.uispec4j.MenuBar;
+import org.uispec4j.Spreadsheet;
+import org.uispec4j.TextBox;
+import org.uispec4j.Trigger;
+import org.uispec4j.UISpec4J;
+import org.uispec4j.UISpecTestCase;
+import org.uispec4j.Window;
+import org.uispec4j.interception.FileChooserHandler;
+import org.uispec4j.interception.WindowHandler;
+
+/**
+ * Bug 583.
+ * Highlighting float value and pressing zero changes to "."
+ */
+public final class UIBug583Test extends UISpecTestCase {
+
+    /**
+     * Initialiser called before each unit test.
+     *
+     * @throws java.lang.Exception When unable to initialise test
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        setAdapter(new MainClassAdapter(OpenSHAPA.class, new String[0]));
+    }
+
+    static {
+      UISpec4J.init();
+    }
+
+    /**
+     * Bug 583 test with a range of values, including 0.
+     *
+     * @throws java.lang.Exception on any error
+     */
+    public void testBug583() throws Exception {
+        String varName = "float";
+        String varType = "FLOAT";
+
+        String root = System.getProperty("testPath");
+        File demoFile = new File(root + "/ui/demo_data.rb");
+        assertTrue(demoFile.exists());
+
+         // Retrieve the components
+        Window window = getMainWindow();
+        MenuBar menuBar = window.getMenuBar();
+
+        // 1. Open and run script to populate database
+        WindowInterceptor
+                .init(menuBar.getMenu("Script").getSubMenu("Run script")
+                    .triggerClick())
+                .process(FileChooserHandler.init()
+                    .assertIsOpenDialog()
+                    .assertAcceptsFilesOnly()
+                    .select(demoFile))
+                .process(new WindowHandler() {
+                    public Trigger process(Window console) {
+                        return console.getButton("Close").triggerClick();
+                    }
+                })
+                .run();
+
+
+        // 2. Get float column
+        Spreadsheet ss = new Spreadsheet((SpreadsheetPanel)
+                (window.getUIComponents(Spreadsheet.class)[0]
+                .getAwtComponent()));
+
+        Vector<Cell> cells = ss.getSpreadsheetColumn(varName).getCells();
+        assertTrue(cells.size() > 0);
+        // 3. Select all and press 0 for each cell.
+        for (Cell c : cells) {
+            c.selectAllAndTypeKey(Cell.VALUE, Key.d0);
+            TextBox t = c.getValue();
+            //BugzID:747 - assertTrue(t.getText().equalsIgnoreCase("0.0"));
+        }
+    }
+}
+
---
+++ src/test/java/org/openshapa/uitests/UIBug661Test.java
@@ -0,0 +1,85 @@
+package org.openshapa.uitests;
+
+import java.io.File;
+import org.uispec4j.interception.MainClassAdapter;
+import org.uispec4j.interception.WindowInterceptor;
+import org.openshapa.OpenSHAPA;
+import org.uispec4j.MenuBar;
+import org.uispec4j.UISpec4J;
+import org.uispec4j.UISpecTestCase;
+import org.uispec4j.Window;
+import org.uispec4j.interception.FileChooserHandler;
+
+/**
+ * Bug 661 Test
+ * Make sure that the open dialog remembers previous open location.
+ */
+public final class UIBug661Test extends UISpecTestCase {
+
+    /**
+     * Initialiser called before each unit test.
+     *
+     * @throws java.lang.Exception When unable to initialise test
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        setAdapter(new MainClassAdapter(OpenSHAPA.class, new String[0]));
+    }
+
+    static {
+      UISpec4J.init();
+    }
+
+    /**
+     * Open a file and maybe check what open dialog opens to.
+     * @param openFile file to open
+     * @param currDirectory currDirectory or null if not testing
+     * @throws Exception on any error
+     */
+    private void openFileAndTest(final String openFile,
+            final File currDirectory) throws Exception {
+        // 1. Retrieve the components
+        Window window = getMainWindow();
+        MenuBar menuBar = window.getMenuBar();
+
+        String root = System.getProperty("testPath") + "ui/";
+        File openCSV = new File(root + openFile);
+        assertTrue(openCSV.exists());
+
+        // 2. Open file in new location
+        if (currDirectory == null) {
+            WindowInterceptor
+                    .init(menuBar.getMenu("File").getSubMenu("Open...").triggerClick())
+                    .process(FileChooserHandler.init()
+                        .assertIsOpenDialog()
+                        .select(openCSV))
+                    .run();
+        } else {
+            WindowInterceptor
+                    .init(menuBar.getMenu("File").getSubMenu("Open...").triggerClick())
+                    .process(FileChooserHandler.init()
+                        .assertIsOpenDialog()
+                        .assertCurrentDirEquals(currDirectory)
+                        .select(openCSV))
+                    .run();
+        }
+        window.dispose();
+    }
+
+    /**
+     * Tests open dialog location
+     * @throws Exception on any error
+     */
+    public void testOpenDialogLocation() throws Exception {
+        String root = System.getProperty("testPath");
+        File location1 = new File(root + "ui/location1");
+        File location2 = new File(root + "ui/location2");
+
+        openFileAndTest("location1/test.shapa", null);
+        //At this point it should remember location1
+        openFileAndTest("location2/test.shapa", location1);
+        //At this point it should remember location2
+        openFileAndTest("location1/test.shapa", location2);
+    }
+}
