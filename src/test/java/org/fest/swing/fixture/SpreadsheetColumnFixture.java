8e43642ec16bd34e8170cbefea05df926c40e652
Douglas Teoh <douglas.teoh@nicta.com.au>
2010-02-11 10:04:03 +1000

    BugzID:1112 - LeftRightCreateCellTest

--- src/test/java/org/fest/swing/fixture/SpreadsheetColumnFixture.java
+++ src/test/java/org/fest/swing/fixture/SpreadsheetColumnFixture.java
@@ -1,58 +1,62 @@
 package org.fest.swing.fixture;
 
 import java.util.Vector;
+
 import javax.swing.JLabel;
+
 import org.fest.swing.core.Robot;
 import org.openshapa.views.discrete.ColumnHeaderPanel;
 import org.openshapa.views.discrete.SpreadsheetCell;
 import org.openshapa.views.discrete.SpreadsheetColumn;
 
 /**
- *
  * @author mmuthukrishna
  */
 public class SpreadsheetColumnFixture {
     private SpreadsheetColumn ssColumn;
     Robot r;
 
-    public SpreadsheetColumnFixture(Robot robot, SpreadsheetColumn spreadsheetColumn) {
+    public SpreadsheetColumnFixture(Robot robot,
+            SpreadsheetColumn spreadsheetColumn) {
         ssColumn = spreadsheetColumn;
         r = robot;
     }
 
     public String getColumnName() {
-         String headerText = ((ColumnHeaderPanel)
-                (ssColumn.getHeaderPanel())).getText();
-        String headerName = headerText.substring(0,
-                headerText.lastIndexOf("  ("));
+        String headerText =
+                ((ColumnHeaderPanel) (ssColumn.getHeaderPanel())).getText();
+        String headerName =
+                headerText.substring(0, headerText.lastIndexOf("  ("));
         return headerName;
     }
 
-     /**
+    /**
      * @return String type of column
      */
     public final String getColumnType() {
-        String headerText = ((ColumnHeaderPanel)
-                (ssColumn.getHeaderPanel())).getText();
-        String headerType = headerText.substring(
-                headerText.lastIndexOf("(") + 1, headerText.length() - 1);
+        String headerText =
+                ((ColumnHeaderPanel) (ssColumn.getHeaderPanel())).getText();
+        String headerType =
+                headerText.substring(headerText.lastIndexOf("(") + 1,
+                        headerText.length() - 1);
         return headerType;
     }
 
     public SpreadsheetCellFixture cell(int id) {
         Vector<SpreadsheetCell> colCells = ssColumn.getCells();
-        return new SpreadsheetCellFixture(r, colCells.elementAt(id-1));
+        return new SpreadsheetCellFixture(r, colCells.elementAt(id - 1));
     }
 
     public JLabelFixture header() {
-        return new JLabelFixture(r, (JLabel)ssColumn.getHeaderPanel());
+        return new JLabelFixture(r, (JLabel) ssColumn.getHeaderPanel());
     }
 
     public Vector<SpreadsheetCellFixture> allCells() {
         Vector<SpreadsheetCell> cells = ssColumn.getCells();
-        Vector<SpreadsheetCellFixture> result = new Vector<SpreadsheetCellFixture>();
+        Vector<SpreadsheetCellFixture> result =
+                new Vector<SpreadsheetCellFixture>();
 
-        for(SpreadsheetCell c : cells) {
+        for (SpreadsheetCell c : cells) {
             result.add(new SpreadsheetCellFixture(r, c));
         }
         return result;
@@ -60,6 +64,14 @@ public class SpreadsheetColumnFixture {
 
     public int numOfCells() {
         return ssColumn.getCells().size();
+    }
+
+    /**
+     * Click on the column header.
+     */
+    public void clickHeader() {
+        JLabelFixture labelFixture = header();
+        labelFixture.click();
     }
 
 }
