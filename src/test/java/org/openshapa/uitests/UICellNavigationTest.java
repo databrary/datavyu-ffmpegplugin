e19b7e917eae9c3c0db5559137727683f3670eec
Michael Muthukrishna <michael.muthukrishna@nicta.com.au>
2010-03-15 13:44:35 +1000

    BugzID:1502 - Test written

--- src/test/java/org/openshapa/uitests/UICellNavigationTest.java
+++ src/test/java/org/openshapa/uitests/UICellNavigationTest.java
@@ -3,6 +3,8 @@ package org.openshapa.uitests;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.fest.swing.fixture.DialogFixture;
 import org.fest.swing.fixture.JFileChooserFixture;
@@ -25,7 +27,7 @@ public final class UICellNavigationTest extends OpenSHAPATestClass {
     /**
      * Test movement left to right using key presses.
      */
-    @Test
+    //@Test
     public void testLeftRightCellNavigation() {
         System.err.println(new Exception().getStackTrace()[0].getMethodName());
         String root = System.getProperty("testPath");
@@ -93,6 +95,82 @@ public final class UICellNavigationTest extends OpenSHAPATestClass {
             Assert.assertTrue(spreadsheet.column(expectedCol)
                     .cell(1).isSelected());
             currSelCol++;
+        }
+    }
+
+    /**
+     * Test movement up down for text cells with multiple lines.
+     */
+    @Test
+    public void testUpDownMultilineCells() {
+        System.err.println(new Exception().getStackTrace()[0].getMethodName());
+        String root = System.getProperty("testPath");
+        File demoFile = new File(root + "/ui/multiline-text.rb");
+        Assert.assertTrue(demoFile.exists());
+
+        // 1. Run script to populate
+        if (Platform.isOSX()) {
+            new RunScriptC(demoFile.toString());
+        } else {
+            mainFrameFixture.clickMenuItemWithPath("Script", "Run script");
+
+            JFileChooserFixture jfcf = mainFrameFixture.fileChooser();
+            jfcf.selectFile(demoFile).approve();
+        }
+
+        // Close script console
+        DialogFixture scriptConsole = mainFrameFixture.dialog();
+        scriptConsole.button("closeButton").click();
+
+        // 2. Get the spreadsheet, check that cells do exist
+        JPanelFixture jPanel = UIUtils.getSpreadsheet(mainFrameFixture);
+        SpreadsheetPanelFixture spreadsheet =
+                new SpreadsheetPanelFixture(mainFrameFixture.robot,
+                        (SpreadsheetPanel) jPanel.component());
+
+        Assert.assertTrue(spreadsheet.allColumns().size() > 0,
+                "Expecting columns to exist.");
+        for (SpreadsheetColumnFixture column : spreadsheet.allColumns()) {
+            Assert.assertTrue(column.numOfCells() > 0,
+                    "Expecting cells to exist.");
+        }
+
+        // 3. Get column
+        SpreadsheetColumnFixture col = spreadsheet.column(0);
+
+        // 4. Select the 6th cell (10 cells in total)
+       col.cell(6).borderSelectCell(true);
+
+        // 5. Move all the way DOWN, then try to go further
+        int currSelCell = 6;
+        while (!col.cell(10).isSelected()) {
+            if (currSelCell % 2 == 0) {
+                spreadsheet.robot.pressAndReleaseKey(KeyEvent.VK_DOWN);
+            } else {
+                for (int i = 0; i < 5; i++) {
+                    spreadsheet.robot.pressAndReleaseKey(KeyEvent.VK_DOWN);
+                }
+            }
+
+            int expectedCell = Math.min(10, currSelCell + 1);
+            Assert.assertTrue(col.cell(expectedCell).isSelected());
+            currSelCell++;
+        }
+
+        // 6. Move all the way UP, then try to go further
+        currSelCell = 10;
+        while (!col.cell(1).isSelected()) {
+            if (currSelCell % 2 == 0) {
+                spreadsheet.robot.pressAndReleaseKey(KeyEvent.VK_UP);
+            } else {
+                for (int i = 0; i < 5; i++) {
+                    spreadsheet.robot.pressAndReleaseKey(KeyEvent.VK_UP);
+                }
+            }
+
+            int expectedCell = Math.max(1, currSelCell - 1);
+            Assert.assertTrue(col.cell(expectedCell).isSelected());
+            currSelCell--;
         }
     }
 }
