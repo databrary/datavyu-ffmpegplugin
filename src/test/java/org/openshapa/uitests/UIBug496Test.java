b9670313c92dbb885b9dde8d2deeac4b8f5c201b
Douglas Teoh <douglas.teoh@nicta.com.au>
2010-03-16 10:15:31 +1000

    BugzID:496 - Updated test to print its name.

--- src/test/java/org/openshapa/uitests/UIBug496Test.java
+++ src/test/java/org/openshapa/uitests/UIBug496Test.java
@@ -25,6 +25,7 @@ public final class UIBug496Test extends OpenSHAPATestClass {
      */
     @Test
     public void testBug496() throws Exception {
+        System.err.println(new Exception().getStackTrace()[0].getMethodName());
         String varName = "i";
         String varType = "INTEGER";
         String varRadio = varType.toLowerCase() + "TypeButton";
