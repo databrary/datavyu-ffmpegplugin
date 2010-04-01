cc0f3cf4342a746562c76bbbc93234bdfee2a5b8
Michael Muthukrishna <michael.muthukrishna@nicta.com.au>
2010-04-01 14:14:35 +1000

    BugzID:501 - test for text wrapping

---
+++ src/test/java/org/openshapa/uitests/UIVisualCellTest.java
@@ -0,0 +1,65 @@
+package org.openshapa.uitests;
+
+import java.awt.event.KeyEvent;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+import org.fest.swing.core.KeyPressInfo;
+import org.fest.swing.fixture.JPanelFixture;
+import org.fest.swing.fixture.SpreadsheetCellFixture;
+import org.fest.swing.fixture.SpreadsheetPanelFixture;
+import org.fest.swing.util.Platform;
+
+import org.openshapa.util.UIImageUtils;
+import org.openshapa.util.UIUtils;
+
+import org.openshapa.views.discrete.SpreadsheetPanel;
+import org.testng.Assert;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * Test the visual aspects of cells.
+ */
+public final class UIVisualCellTest extends OpenSHAPATestClass {
+    /**
+     * Checks if multiline text wrapping is working.
+     * @throws IOException if can't load image file
+     */
+    @Test public void testMultilineTextWrapping() throws IOException {
+
+        // Create new text cell
+        JPanelFixture jPanel = UIUtils.getSpreadsheet(mainFrameFixture);
+        SpreadsheetPanelFixture ssPanel = new SpreadsheetPanelFixture(
+                mainFrameFixture.robot, (SpreadsheetPanel) jPanel.component());
+
+        UIUtils.createNewVariable(mainFrameFixture, "t", "text");
+        ssPanel.column("t").click();
+
+        mainFrameFixture.clickMenuItemWithPath("Spreadsheet", "New Cell");
+
+        SpreadsheetCellFixture cell = ssPanel.column(0).cell(1);
+
+        // Reset text zoom
+        mainFrameFixture.clickMenuItemWithPath("Spreadsheet", "Zoom",
+            "Reset Zoom");
+
+        // Add long text that should wrap on multiple lines
+        UIUtils.setClipboard(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at dolor at velit placerat convallis. Cras sit amet dolor vitae tellus egestas ullamcorper ut non risus. Sed ac justo elit, sed cursus ligula. Nullam nec urna ac tortor accumsan placerat. Nam vestibulum, mauris vitae gravida suscipit, magna mauris interdum ligula, vel pulvinar eros arcu sed quam. Vestibulum eget tristique velit. Nulla facilisi. Integer lectus erat, rhoncus sed sagittis vestibulum, vehicula quis turpis. Fusce malesuada tristique massa, in iaculis magna accumsan vel.");
+
+        cell.cellValue().pressAndReleaseKey(KeyPressInfo.keyCode(
+                KeyEvent.VK_V).modifiers(
+                Platform.controlOrCommandMask()));
+
+        String root = System.getProperty("testPath");
+        File refImageFile = new File(root + "/ui/multiline.bmp");
+
+        RenderedImage componentImage = UIImageUtils.captureAsScreenshot(cell.component());
+
+        Assert.assertTrue(UIImageUtils.areImagesEqual(componentImage, refImageFile));
+    }
+
+}
---
+++ src/test/java/org/openshapa/util/UIImageUtils.java
@@ -0,0 +1,123 @@
+package org.openshapa.util;
+
+import java.awt.AWTException;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JComponent;
+
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+
+import javax.imageio.ImageIO;
+
+import org.testng.Assert;
+
+
+/**
+ * Image utilities.
+ * Example: capturing screenshot of component, comparing images.
+ */
+public final class UIImageUtils {
+
+    /** Maximum distance one pixel can be away from another */
+    public static final double MAX_PIXEL_DISTANCE = Math.sqrt(255 ^ (2 + 255)
+            ^ (2 + 255) ^ 2);
+
+
+    public static Boolean areImagesEqual(final RenderedImage uiImage,
+        final File refFile) throws IOException {
+
+        // CONSTANTS
+        // Pixel threshold as a percentage
+        final double PIXEL_THRESHOLD = 0.1;
+        final double ERROR_THRESHOLD = 0.1;
+
+        // Load image from file
+        RenderedImage refImage = ImageIO.read(refFile);
+
+        // Check that images are the same size
+        Assert.assertEquals(uiImage.getHeight(), refImage.getHeight());
+        Assert.assertEquals(uiImage.getWidth(), refImage.getWidth());
+
+        int totalPixels = uiImage.getHeight() * uiImage.getWidth();
+
+        // Number of pixels incorrect
+        int errorPixels = 0;
+
+        // For each pixel, calculate distance
+        for (int i = 0; i < totalPixels; i++) {
+            int r1 = uiImage.getColorModel().getRed(i);
+            int g1 = uiImage.getColorModel().getGreen(i);
+            int b1 = uiImage.getColorModel().getBlue(i);
+            int r2 = refImage.getColorModel().getRed(i);
+            int g2 = refImage.getColorModel().getGreen(i);
+            int b2 = refImage.getColorModel().getBlue(i);
+            double pixelDistance = Math.sqrt(((r1 - r2) * (r1 - r2))
+                    + ((g1 - g2)
+                        * (g1 - g2)) + ((b1 - b2) * (b1 - b2)));
+
+            // Check if correct within threshold
+            if (pixelDistance > (PIXEL_THRESHOLD * MAX_PIXEL_DISTANCE)) {
+                errorPixels++;
+            }
+        }
+
+        // Check if number of error pixels > threshold
+        return (errorPixels / totalPixels) < ERROR_THRESHOLD;
+    }
+
+    /**
+     * Captures screenshot of component and saves to a file.
+     * @param component JComponent to capture screenshot
+     * @param saveAs file name
+     */
+    public static void captureAsScreenshot(final JComponent component,
+        final File saveAs) {
+
+        try {
+            Robot robot = new Robot();
+
+            // Create Rectangle around component
+            Point locOnScreen = component.getLocationOnScreen();
+            Rectangle bounds = component.getBounds();
+            bounds.setLocation(locOnScreen);
+
+            BufferedImage bi = robot.createScreenCapture(bounds);
+            ImageIO.write(bi, "bmp", saveAs);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+    * Captures screenshot of component nd returns as BufferedImage
+    * @param component JComponent to capture screenshot
+    * @param saveAs file name
+    */
+    public static BufferedImage captureAsScreenshot(
+        final JComponent component) {
+        BufferedImage bi = null;
+
+        try {
+            Robot robot = new Robot();
+
+            // Create Rectangle around component
+            Point locOnScreen = component.getLocationOnScreen();
+            Rectangle bounds = component.getBounds();
+            bounds.setLocation(locOnScreen);
+
+            bi = robot.createScreenCapture(bounds);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        return bi;
+    }
+}
