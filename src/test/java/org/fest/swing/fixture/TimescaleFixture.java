8cb4c503b25741c90d43e219e645a67bc78cfbe0
Michael Muthukrishna <michael.muthukrishna@nicta.com.au>
2010-05-31 13:58:54 +1000

    BugzID:1836 - Tests written

--- src/test/java/org/fest/swing/fixture/TimescaleFixture.java
+++ src/test/java/org/fest/swing/fixture/TimescaleFixture.java
@@ -1,13 +1,19 @@
 package org.fest.swing.fixture;
 
+import static org.fest.reflect.core.Reflection.method;
+import static org.fest.reflect.core.Reflection.field;
+
 import java.awt.Point;
 
 import org.fest.swing.core.MouseButton;
 import org.fest.swing.core.Robot;
+import org.openshapa.OpenSHAPA;
 
 import org.openshapa.controllers.component.TimescaleController;
+import org.openshapa.models.component.TimescaleModel;
 
 import org.openshapa.util.UIUtils;
+import org.openshapa.views.OpenSHAPAFileChooser;
 
 
 /**
@@ -32,58 +38,72 @@ public class TimescaleFixture extends ComponentFixture {
     /**
      * @return End time represented as a long.
      */
-    public long getEndTimeAsLong() {
+    public final long getEndTimeAsLong() {
         return timescaleC.getViewableModel().getEnd();
     }
 
     /**
      * @return End time represented as a timestamp.
      */
-    public String getEndTimeAsTimeStamp() {
+    public final String getEndTimeAsTimeStamp() {
         return UIUtils.millisecondsToTimestamp(getEndTimeAsLong());
     }
 
     /**
      * @return Zoom end time represented as a long.
      */
-    public long getZoomEndTimeAsLong() {
+    public final long getZoomEndTimeAsLong() {
         return timescaleC.getViewableModel().getZoomWindowEnd();
     }
 
     /**
      * @return Zoom end time represented as a timestamp.
      */
-    public String getZoomEndTimeAsTimeStamp() {
+    public final String getZoomEndTimeAsTimeStamp() {
         return UIUtils.millisecondsToTimestamp(getZoomEndTimeAsLong());
     }
 
     /**
      * @return Zoom end time represented as a long.
      */
-    public long getZoomStartTimeAsLong() {
+    public final long getZoomStartTimeAsLong() {
         return timescaleC.getViewableModel().getZoomWindowStart();
     }
 
     /**
      * @return Zoom end time represented as a timestamp.
      */
-    public String getZoomStartTimeAsTimeStamp() {
+    public final String getZoomStartTimeAsTimeStamp() {
         return UIUtils.millisecondsToTimestamp(getZoomStartTimeAsLong());
     }
 
-    public void doubleClickAt(final int pixel) {
+    /**
+     * Double click at pixel in component.
+     * @param pixel to click relative to component ie. 0 to width
+     */
+    public final void doubleClickAt(final int pixel) {
         Point topLeft = target.getBounds().getLocation();
-        int width = target.getWidth();
+        int width = getWidth();
         Point click = null;
 
+        TimescaleModel tm = field("timescaleModel").ofType(TimescaleModel.class)
+               .in(timescaleC).get();
+
         if (pixel > width) {
-            click = new Point(topLeft.x + width,
+            click = new Point(topLeft.x + width + tm.getPaddingLeft(),
                     topLeft.y + (target.getHeight() / 2));
         } else {
-            click = new Point(topLeft.x + pixel,
+            click = new Point(topLeft.x + pixel + tm.getPaddingLeft(),
                     topLeft.y + (target.getHeight() / 2));
         }
 
         robot.click(target, click, MouseButton.LEFT_BUTTON, 2);
+    }
+
+    public int getWidth() {
+        TimescaleModel tm = field("timescaleModel").ofType(TimescaleModel.class)
+               .in(timescaleC).get();
+
+        return target.getWidth() - tm.getPaddingLeft() - tm.getPaddingRight();
     }
 }
