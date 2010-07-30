cc1134a7a35053b8cb16a0c2c4cbe68e5ae6d947
Michael Muthukrishna <michael.muthukrishna@nicta.com.au>
2010-07-30 16:28:37 +1000

    BugzID:747 - Test uncommented

--- src/test/java/org/openshapa/uitests/UICellValueTest.java
+++ src/test/java/org/openshapa/uitests/UICellValueTest.java
@@ -12,6 +12,7 @@ import java.util.logging.Logger;
 import javax.swing.text.BadLocationException;
 
 import org.fest.swing.core.KeyPressInfo;
+import org.fest.swing.core.matcher.JTextComponentMatcher;
 import org.fest.swing.fixture.JTextComponentFixture;
 import org.fest.swing.fixture.SpreadsheetCellFixture;
 import org.fest.swing.util.Platform;
@@ -759,6 +760,41 @@ public final class UICellValueTest extends OpenSHAPATestClass {
                 
                 Assert.assertEquals(cell.cellValue().component().getSelectedText(), matrixCol[i]);
             }
+        }
+    }
+
+    /**
+     * Bug 583. Highlighting float value and pressing zero changes to "."
+     * Bug 583 test with a range of values, including 0.
+     */
+    @Test public void testBug583() throws InterruptedException {
+
+        /**
+         * Different cell variable types.
+         */
+        System.err.println(new Exception().getStackTrace()[0].getMethodName());
+
+        String[] floatCellValues = { "0.000000",
+                "0.123400", "0.246800", "0.370200", "0.493600", "0.617000",
+                "0.740400", "0.863800", "0.987200", "1.110600"
+            };
+
+        File demoFile = new File(testFolder + "/ui/demo_data.rb");
+        Assert.assertTrue(demoFile.exists());
+
+        // 1. Run script to populate
+        mainFrameFixture.runScript(demoFile);
+
+        // Close script console
+        mainFrameFixture.closeScriptConsoleOnFinish();
+
+        // 2. Get each float cell
+        for (String floatVal : floatCellValues) {
+            JTextComponentFixture cellValue = mainFrameFixture.textBox(
+                    JTextComponentMatcher.withText(floatVal));
+            cellValue.selectAll();
+            cellValue.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_0));
+            Assert.assertEquals(cellValue.text(), "0.0");
         }
     }
 
