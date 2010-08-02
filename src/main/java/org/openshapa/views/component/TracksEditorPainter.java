f8432f0c8614ab21bad66b86dddbdd207dbedf18
Douglas Teoh <douglas.teoh@nicta.com.au>
2010-08-02 11:58:36 +1000

    BugzID:2086 - Fixed track panel being wider than needed.

--- src/main/java/org/openshapa/views/component/TracksEditorPainter.java
+++ src/main/java/org/openshapa/views/component/TracksEditorPainter.java
@@ -7,21 +7,27 @@ import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
 
-public class TracksEditorPainter extends JPanel {
-	public TracksEditorPainter() {
-		//TODO need to get these numbers from the MixerControllerV class
-		setLayout(new MigLayout("wrap, ins 0", "[685]", "[70]"));
-		setOpaque(false);
-	}
-	
-	public void paint(Graphics g) {
-		g.setColor(Color.white);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		
-		g.setColor(new Color(237, 237, 237));
-		//TODO need to get these numbers from the MixerControllerV class
-		g.fillRect(101, 0, 654, getHeight());
-		
-		super.paint(g);
-	}
+
+public final class TracksEditorPainter extends JPanel {
+
+    private static final Color PANEL_BG_COLOR = Color.WHITE;
+
+    private static final Color TRACK_BG_COLOR = new Color(237, 237, 237);
+
+    public TracksEditorPainter() {
+        setLayout(new MigLayout("wrap, ins 0", "", ""));
+        setOpaque(false);
+    }
+
+    public void paint(final Graphics g) {
+        g.setColor(PANEL_BG_COLOR);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        g.setColor(TRACK_BG_COLOR);
+
+        //TODO need to get these numbers from the MixerControllerV class
+        g.fillRect(101, 0, 654, getHeight());
+
+        super.paint(g);
+    }
 }
