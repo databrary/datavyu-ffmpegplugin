38e801a9cf0b6458ed74cdced0ec711d89c45087
Douglas Teoh <douglas.teoh@nicta.com.au>
2010-11-29 11:47:14 +1000

    BugzID:2470 - Patch add data Quaqua FC to include plugin chooser.

--- src/main/java/org/openshapa/views/MacOSJFC.java
+++ src/main/java/org/openshapa/views/MacOSJFC.java
@@ -1,34 +1,72 @@
 package org.openshapa.views;
 
 import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
+import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class MacOSJFC extends BaseJFC {
-    @Override protected JDialog createDialog(final Component parent) {
+    @Override
+    protected JDialog createDialog(final Component parent) {
         JDialog dialog = super.createDialog(parent);
 
         JLabel pluginSelect = new JLabel();
         pluginSelect.setText("Plugin:");
+        pluginSelect.setHorizontalAlignment(SwingConstants.RIGHT);
 
         pluginsBox = new JComboBox(plugins.toArray());
         pluginsBox.setEditable(false);
         pluginsBox.setLightWeightPopupEnabled(true);
-        pluginsBox.setSize(220, 27);
 
-        JPanel bottomPanel = (JPanel) getComponent(4);
-        JPanel inputPanel = (JPanel) bottomPanel.getComponent(0);
+        JPanel pluginPanel = new JPanel(new GridBagLayout());
+        pluginPanel.setBorder(BorderFactory.createEmptyBorder(6, 0, 0, 0));
+        {
+            GridBagConstraints c = new GridBagConstraints();
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridy = 0;
+            c.weightx = 0.5D;
+            c.insets = new Insets(0, 0, 0, 6);
 
+            pluginPanel.add(pluginSelect, c);
+        }
+        {
+            GridBagConstraints c = new GridBagConstraints();
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 1;
+            c.gridy = 0;
+            c.weightx = 0;
+
+            pluginPanel.add(pluginsBox, c);
+        }
+        {
+            GridBagConstraints c = new GridBagConstraints();
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 2;
+            c.gridy = 0;
+            c.weightx = 0.5D;
+            c.insets = new Insets(0, 6, 0, 0);
+
+            JPanel blankPanel = new JPanel();
+            blankPanel.setBorder(null);
+            pluginPanel.add(blankPanel, c);
+        }
+
+        JPanel inputPanel = (JPanel) getComponent(2);
         Component[] origs = inputPanel.getComponents();
-        inputPanel.add(pluginSelect);
-        inputPanel.add(pluginsBox);
+
+        inputPanel.removeAll();
         inputPanel.add(origs[0]);
         inputPanel.add(origs[1]);
-
-        dialog.setSize(getWidth() + 100, getHeight() + 50);
+        inputPanel.add(pluginPanel);
+        inputPanel.add(origs[2]);
 
         return dialog;
     }
