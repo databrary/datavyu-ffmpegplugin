af2eb30469251e1905418c73a63d7c4815d2db2e
Douglas Teoh <douglas.teoh@nicta.com.au>
2010-12-13 09:35:13 +1000

    LOLWTF TESTNG METHODS DONT EXIST???

--- src/test/java/org/openshapa/uitests/UICreateCellTest.java
+++ src/test/java/org/openshapa/uitests/UICreateCellTest.java
@@ -1,14 +1,13 @@
 package org.openshapa.uitests;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
 import org.fest.swing.fixture.SpreadsheetColumnFixture;
 import org.fest.swing.util.Platform;
-
 import org.openshapa.util.UIUtils;
-
-import org.testng.Assert;
-
 import org.testng.annotations.Test;
-
 
 /**
  * Tests creating cells to left and right of a cell.
@@ -18,29 +17,30 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
     /**
      * Test creating new cells with the new cell button.
      */
-    @Test public void testCreateNewCellWithButton() {
+    @Test
+    public void testCreateNewCellWithButton() {
         printTestName();
 
-        //Create column of each type
+        // Create column of each type
         for (String var : UIUtils.VAR_TYPES) {
             mainFrameFixture.createNewVariable(var.substring(0, 1), var);
         }
 
         spreadsheet = mainFrameFixture.getSpreadsheet();
-        Assert.assertEquals(spreadsheet.allColumns().size(),
-            UIUtils.VAR_TYPES.length);
+        assertEquals(UIUtils.VAR_TYPES.length, spreadsheet.allColumns().size());
 
-        //Create cell for each column
+        // Create cell for each column
         for (SpreadsheetColumnFixture col : spreadsheet.allColumns()) {
             col.pressNewCellButton();
-            Assert.assertEquals(col.allCells().size(), 1);
+            assertEquals(1, col.allCells().size());
         }
     }
 
     /**
      * Test creating cells to the left and right.
      */
-    @Test public void testCreateCellLeftRight() {
+    @Test
+    public void testCreateCellLeftRight() {
         printTestName();
 
         // Left column
@@ -67,8 +67,7 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
         mainFrameFixture.clickMenuItemWithPath("Spreadsheet", "New Cell");
 
         // 3. Check if the new cell has been created.
-        Assert.assertTrue(cellExists("C", 1),
-            "Expecting centre cell to be created.");
+        assertTrue("Expecting centre cell to be created.", cellExists("C", 1));
 
         final String centerOnset = "11:23:58:132";
         final String centerOffset = "12:34:56:789";
@@ -84,17 +83,16 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
 
         // 2. Make a new cell to the left of the center cell
         mainFrameFixture.clickMenuItemWithPath("Spreadsheet",
-            "New Cell to Left");
+                "New Cell to Left");
 
         // 3. Check if the new cell has been created.
-        Assert.assertTrue(cellExists("L", 1),
-            "Expecting left cell to be created.");
+        assertTrue("Expecting left cell to be created.", cellExists("L", 1));
 
         // 4. Check that onset and offset values have been inherited
-        Assert.assertTrue(cellHasOnset("L", 1, centerOnset),
-            "Expecting left cell to inherit centre cell onset value.");
-        Assert.assertTrue(cellHasOffset("L", 1, centerOffset),
-            "Expecting left cell to inherit centre cell offset value.");
+        assertTrue("Expecting left cell to inherit centre cell onset value.",
+                cellHasOnset("L", 1, centerOnset));
+        assertTrue("Expecting left cell to inherit centre cell offset value.",
+                cellHasOffset("L", 1, centerOffset));
 
         /*
          * Create a cell to the right of the center cell
@@ -105,23 +103,23 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
 
         // 2. Make a new cell to the left of the center cell
         mainFrameFixture.clickMenuItemWithPath("Spreadsheet",
-            "New Cell to Right");
+                "New Cell to Right");
 
         // 3. Check if the new cell has been created.
-        Assert.assertTrue(cellExists("R", 1),
-            "Expecting right cell to be created.");
+        assertTrue("Expecting right cell to be created.", cellExists("R", 1));
 
         // 4. Check that onset and offset values have been inherited
-        Assert.assertTrue(cellHasOnset("R", 1, centerOnset),
-            "Expecting right cell to inherit centre cell onset value.");
-        Assert.assertTrue(cellHasOffset("R", 1, centerOffset),
-            "Expecting right cell to inherit centre cell offset value.");
+        assertTrue("Expecting right cell to inherit centre cell onset value.",
+                cellHasOnset("R", 1, centerOnset));
+        assertTrue("Expecting right cell to inherit centre cell offset value.",
+                cellHasOffset("R", 1, centerOffset));
     }
 
     /**
      * Test for bug 698.
      */
-    @Test public void testBug698() {
+    @Test
+    public void testBug698() {
         printTestName();
 
         // Left column
@@ -160,12 +158,12 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
         // 2. Make a new cells to the left
         mainFrameFixture.menuItemWithPath("Spreadsheet").click();
         mainFrameFixture.clickMenuItemWithPath("Spreadsheet",
-            "New Cells to Left");
+                "New Cells to Left");
 
         // 3. Check that new cells have been created
-        Assert.assertEquals(spreadsheet.column("L").allCells().size(), 2);
-        Assert.assertEquals(spreadsheet.column("C").allCells().size(), 2);
-        Assert.assertEquals(spreadsheet.column("R").allCells().size(), 1);
+        assertEquals(2, spreadsheet.column("L").allCells().size());
+        assertEquals(2, spreadsheet.column("C").allCells().size());
+        assertEquals(1, spreadsheet.column("R").allCells().size());
 
         /*
          * Select all cells and try to create a cell to the right.
@@ -182,12 +180,12 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
         // 2. Make a new cell to the right
         mainFrameFixture.menuItemWithPath("Spreadsheet").click();
         mainFrameFixture.clickMenuItemWithPath("Spreadsheet",
-            "New Cells to Right");
+                "New Cells to Right");
 
         // 3. Check that new cells have been created
-        Assert.assertEquals(spreadsheet.column("L").allCells().size(), 2);
-        Assert.assertEquals(spreadsheet.column("C").allCells().size(), 4);
-        Assert.assertEquals(spreadsheet.column("R").allCells().size(), 3);
+        assertEquals(2, spreadsheet.column("L").allCells().size());
+        assertEquals(4, spreadsheet.column("C").allCells().size());
+        assertEquals(3, spreadsheet.column("R").allCells().size());
     }
 
     /**
@@ -201,11 +199,11 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
      * @return boolean - true if has onset
      */
     private boolean cellHasOnset(final String varName, final int id,
-        final String onset) {
+            final String onset) {
         spreadsheet = mainFrameFixture.getSpreadsheet();
 
         return spreadsheet.column(varName).cell(id).onsetTimestamp().text()
-            .equals(onset);
+                .equals(onset);
     }
 
     /**
@@ -219,11 +217,11 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
      * @return boolean - true if has offset
      */
     private boolean cellHasOffset(final String varName, final int id,
-        final String offset) {
+            final String offset) {
         spreadsheet = mainFrameFixture.getSpreadsheet();
 
         return spreadsheet.column(varName).cell(id).offsetTimestamp().text()
-            .equals(offset);
+                .equals(offset);
     }
 
     /**
@@ -236,7 +234,7 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
      *            Should be in the format HH:mm:ss:SSS
      */
     private void changeOnset(final String varName, final int id,
-        final String onset) {
+            final String onset) {
         spreadsheet = mainFrameFixture.getSpreadsheet();
 
         spreadsheet.column(varName).cell(id).onsetTimestamp().enterText(onset);
@@ -252,11 +250,11 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
      *            Should be in the format HH:mm:ss:SSS
      */
     private void changeOffset(final String varName, final int id,
-        final String offset) {
+            final String offset) {
         spreadsheet = mainFrameFixture.getSpreadsheet();
 
-        spreadsheet.column(varName).cell(id).offsetTimestamp().enterText(
-            offset);
+        spreadsheet.column(varName).cell(id).offsetTimestamp()
+                .enterText(offset);
     }
 
     /**
@@ -287,7 +285,7 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
 
     /**
      * Creates a new variable and checks that it has been created.
-     *
+     * 
      * @param varName
      *            variable name
      * @param varType
@@ -298,10 +296,10 @@ public final class UICreateCellTest extends OpenSHAPATestClass {
 
         // 1. Check that the column exists.
         SpreadsheetColumnFixture col = spreadsheet.column(varName);
-        Assert.assertNotNull(col, "Expecting column to exist.");
-        Assert.assertEquals(col.getColumnType(), varType);
+        assertNotNull("Expecting column to exist.", col);
+        assertEquals(varType, col.getColumnType());
 
         // 2. Check that column has no cells
-        Assert.assertTrue(col.numOfCells() == 0, "Expecting no cells.");
+        assertTrue("Expecting no cells.", col.numOfCells() == 0);
     }
 }
