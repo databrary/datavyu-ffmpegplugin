3e7c92b4ebfcb51d88a053c671509ae892b7222f
Clinton <clinton.freeman@nicta.com.au>
2011-03-21 10:22:37 +1000

    BugzID:2527 - Deleting cells no longer requires a full spreadsheet refresh.

--- src/main/java/org/openshapa/controllers/DeleteCellC.java
+++ src/main/java/org/openshapa/controllers/DeleteCellC.java
@@ -1,7 +1,6 @@
 package org.openshapa.controllers;
 
 import com.usermetrix.jclient.Logger;
-import java.util.Vector;
 
 import org.openshapa.OpenSHAPA;
 
@@ -9,11 +8,10 @@ import org.openshapa.models.db.legacy.DataCell;
 import org.openshapa.models.db.legacy.MacshapaDatabase;
 import org.openshapa.models.db.legacy.SystemErrorException;
 
-import org.openshapa.views.OpenSHAPAView;
 import org.openshapa.views.discrete.SpreadsheetPanel;
 
 import com.usermetrix.jclient.UserMetrix;
-
+import java.util.List;
 
 /**
  * Controller for deleting cells from the database.
@@ -21,21 +19,21 @@ import com.usermetrix.jclient.UserMetrix;
 public final class DeleteCellC {
 
     /** The logger for this class. */
-    private Logger logger = UserMetrix.getLogger(DeleteColumnC.class);
+    private static final Logger LOGGER = UserMetrix.getLogger(DeleteColumnC.class);
 
     /**
      * Constructor.
      *
-     * @param cellsToDelete
-     *            The cells to delete from the spreadsheet.
+     * @param cellsToDelete The cells to delete from the spreadsheet.
      */
-    public DeleteCellC(final Vector<DataCell> cellsToDelete) {
-        logger.usage("delete cells");
+    public DeleteCellC(final List<DataCell> cellsToDelete) {
+        LOGGER.usage("delete cells");
 
         // The spreadsheet is the view for this controller.
         SpreadsheetPanel view = (SpreadsheetPanel) OpenSHAPA.getApplication()
-            .getMainView().getComponent();
-        MacshapaDatabase model = OpenSHAPA.getProjectController().getLegacyDB().getDatabase();
+                                                   .getMainView().getComponent();
+        MacshapaDatabase model = OpenSHAPA.getProjectController()
+                                          .getLegacyDB().getDatabase();
         view.deselectAll();
 
         try {
@@ -43,29 +41,22 @@ public final class DeleteCellC {
 
                 // Check if the cell we are deleting is the last created cell.
                 // Default this back to 0 if it is.
-                if (c.getID()
-                        == OpenSHAPA.getProjectController()
-                        .getLastCreatedCellId()) {
+                if (c.getID() == OpenSHAPA.getProjectController()
+                                          .getLastCreatedCellId()) {
                     OpenSHAPA.getProjectController().setLastCreatedCellId(0);
                 }
 
                 // Check if the cell we are deleting is the last selected cell.
                 // Default this back to 0 if it is.
-                if (c.getID()
-                        == OpenSHAPA.getProjectController()
-                        .getLastSelectedCellId()) {
+                if (c.getID() == OpenSHAPA.getProjectController()
+                                          .getLastSelectedCellId()) {
                     OpenSHAPA.getProjectController().setLastSelectedCellId(0);
                 }
 
                 model.removeCell(c.getID());
             }
-
-            OpenSHAPAView v = (OpenSHAPAView) OpenSHAPA.getApplication()
-                .getMainView();
-            v.showSpreadsheet();
-
         } catch (SystemErrorException e) {
-            logger.error("Unable to delete cells", e);
+            LOGGER.error("Unable to delete cells", e);
         }
     }
 }
