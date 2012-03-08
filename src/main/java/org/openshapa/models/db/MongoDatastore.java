6c3866179fd4819aa1c793a8e10ecd572a703e93
Jesse Lingeman <jmlingeman@gmail.com>
2012-03-08 18:17:32 -0500

    DB listeners are now updated properly.

--- src/main/java/org/openshapa/models/db/MongoDatastore.java
+++ src/main/java/org/openshapa/models/db/MongoDatastore.java
@@ -1,17 +1,26 @@
-/**
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+/*
+ * Copyright (c) 2011 OpenSHAPA Foundation, http://openshapa.org
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package org.openshapa.models.db;
 
 import com.mongodb.BasicDBObject;
@@ -29,6 +38,7 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 import org.openshapa.util.NativeLoader;
 
 /**
@@ -48,11 +58,13 @@ public class MongoDatastore implements Datastore {
     
     // Only a single instance of the mongo DB exists.
     private static DB mongoDB = null;
+    
+    private Vector<DatastoreListener> dbListeners = new Vector<DatastoreListener>();
 
     public MongoDatastore () {
 
     }
-
+    
     /**
      * Spin up the mongo instance so that we can query and do stuff with it.
      */
@@ -234,6 +246,10 @@ public class MongoDatastore implements Datastore {
         Variable v = new MongoVariable(name, new Argument("arg01", type));
         
         varCollection.save((MongoVariable)v);
+        
+        for(DatastoreListener dbl : this.dbListeners ) {
+            dbl.variableAdded(v);
+        }
 
         return v;
     }
@@ -241,14 +257,23 @@ public class MongoDatastore implements Datastore {
     @Override
     @Deprecated
     public void addVariable(final Variable var) {
+
     }
 
     @Override
     public void removeVariable(final Variable var) {
+        DBCollection varCollection = mongoDB.getCollection("variables");
+
+        varCollection.remove((MongoVariable)var);
+
+        for(DatastoreListener dbl : this.dbListeners ) {
+            dbl.variableRemoved(var);
+        }
     }
 
     @Override
     public void removeCell(final Cell cell) {
+        getVariable(cell).removeCell(cell);
     }
 
     @Override
@@ -279,9 +304,13 @@ public class MongoDatastore implements Datastore {
 
     @Override
     public void addListener(final DatastoreListener listener) {
+        dbListeners.add(listener);
     }
 
     @Override
     public void removeListener(final DatastoreListener listener) {
+        if(dbListeners.contains(listener)) {
+            dbListeners.remove(listener);
+        }
     }
 }
