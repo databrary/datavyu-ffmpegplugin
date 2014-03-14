1702d9357195a198658198ec66a8006931b0fa54
Jon Coe <jonmcoe@gmail.com>
2014-03-14 18:13:31 -0400

    Shell script one-liners for windows and linux compilation

---
+++ compile_linuxjar.sh
@@ -0,0 +1,1 @@
+mvn -Prelease,jar-package -Dmaven.test.skip=true clean package jar:jar assembly:assembly
---
+++ compile_win.sh
@@ -0,0 +1,1 @@
+mvn -Prelease,win-package -Dmaven.test.skip=true clean package jar:jar launch4j:launch4j assembly:assembly
