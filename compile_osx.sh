8690f4994c3b6e7a81a82a6fb08655d486ccc9ea
smidget <jmlingeman@gmail.com>
2015-06-30 18:13:49 -0400

    Updated compile script

--- compile_osx.sh
+++ compile_osx.sh
@@ -1,2 +1,2 @@
 curl http://www.datavyu.org/docs/user-guide.pdf > packaged_docs/user-guide.pdf
-mvn clean -U -Prelease -Dmaven.test.skip=true package osxappbundle:bundle
+mvn clean -U -Dmaven.test.skip=true jfx:native
