728da9ae6281b0b4c32474929c074cd3b723db17
Reda Nezzar <nezzar.reda@gmail.com>
2018-11-19 16:16:32 -0500

    Add NativeLibraryLoader and SimpleJavaMediaPlayer

--- src/main/java/org/datavyu/plugins/ffmpeg/examples/SimpleJavaMediaPlayer.java
+++ src/main/java/org/datavyu/plugins/ffmpeg/examples/SimpleJavaMediaPlayer.java
@@ -1,5 +1,6 @@
 package org.datavyu.plugins.ffmpeg.examples;
 
+
 import org.datavyu.plugins.ffmpeg.*;
 
 import javax.swing.*;
@@ -7,29 +8,15 @@ import java.io.File;
 import java.net.URI;
 
 public class SimpleJavaMediaPlayer {
-
     public static void main(String[] args) {
         // Define the media file
         URI mediaPath = new File("Nature_30fps_1080p.mp4").toURI();
-        //URI mediaPath = new File("counter.mp4").toURI();
-        //URI mediaPath = new File("DatavyuSampleVideo.mp4").toURI();
 
         // Create the media player using the constructor with File
         MediaPlayerData mediaPlayer = new FfmpegJavaMediaPlayer(mediaPath, new JDialog());
 
-        mediaPlayer.addMediaErrorListener(new MediaErrorListener() {
-            @Override
-            public void onError(Object source, int errorCode, String message) {
-                System.err.println(errorCode + ": " + message);
-            }
-        });
-
         // Initialize the player
         mediaPlayer.init();
-
-        // Set initial time and rate (which in the past triggered a flush on the queue and thus inaccurate timing)
-        mediaPlayer.setStartTime(0);
-        mediaPlayer.setRate(1f);
 
         // Open a JFrame to control the media player through key commands
         new JMediaPlayerControlFrame(mediaPlayer);
