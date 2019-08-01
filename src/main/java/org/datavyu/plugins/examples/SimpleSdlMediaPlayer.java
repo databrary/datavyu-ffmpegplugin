bbf3e4880acccea44e71e56cc450dfc6f1adb110
Reda Nezzar <nezzar.reda@gmail.com>
2019-08-01 16:03:23 -0400

    Put Correct file name in SDL example #188

--- src/main/java/org/datavyu/plugins/examples/SimpleSdlMediaPlayer.java
+++ src/main/java/org/datavyu/plugins/examples/SimpleSdlMediaPlayer.java
@@ -11,7 +11,7 @@ import java.net.URI;
 public class SimpleSdlMediaPlayer {
   public static void main(String[] args) {
     // Define the media file
-    URI mediaPath = new File("Nature_30fp.mp4").toURI();
+    URI mediaPath = new File("Nature_30fps_1080p.mp4").toURI();
 
     // Create the media player using the constructor with File
     MediaPlayer mediaPlayer = new FfmpegSdlMediaPlayer(mediaPath);
