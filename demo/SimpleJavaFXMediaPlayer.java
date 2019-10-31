4fa5c21ae717110917e6d56cb7981835d0ca50af
Reda Nezzar <nezzar.reda@gmail.com>
2019-10-31 17:56:00 -0400

    Decouple FFmpegSdlMediaPLayer from JavaFX main thread #227

--- demo/SimpleJavaFXMediaPlayer.java
+++ demo/SimpleJavaFXMediaPlayer.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,39 +9,65 @@ import org.datavyu.plugins.ffmpeg.FfmpegSdlMediaPlayer;
 
 import java.io.File;
 import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-/**
- * WARNING: This class is tested only on Windows platform
- */
+/** WARNING: This class is tested only on Windows platform */
 public class SimpleJavaFXMediaPlayer extends Application {
   private static Logger logger = LogManager.getLogger(SimpleJavaFXMediaPlayer.class);
   private JMediaPlayerControlFrame controller;
+  private MediaPlayer mediaPlayer;
+  private URI mediaPath;
 
   @Override
   public void start(Stage primaryStage) {
-    URI mediaPath = new File("counter.mp4").toURI();
-    MediaPlayer mediaPlayer = new FfmpegSdlMediaPlayer(mediaPath);
+    mediaPath = new File("counter.mp4").toURI();
 
-    mediaPlayer.addMediaErrorListener(
-        // Handle error thrown by The Media Player
-        (source, errorCode, message) -> logger.error(errorCode + ": " + message));
+    MediaPlayerTask mediaPlayerTask = new MediaPlayerTask(mediaPath);
 
-    mediaPlayer.init();
+    mediaPlayerTask.setOnSucceeded(
+        event -> {
+          mediaPlayer = mediaPlayerTask.getValue();
 
-    // Open a simple JFrame to control the media player through key commands
-    // Be creative and create your own controller in JavaFX
-    Platform.runLater(
-        () -> {
-          controller = new JMediaPlayerControlFrame(mediaPlayer);
+          mediaPlayer.addMediaErrorListener(
+              // Handle error thrown by The Media Player
+              (source, errorCode, message) -> logger.error(errorCode + ": " + message));
+
+          // Handle Window Key events triggered from SDL window
+          mediaPlayer.addSdlKeyEventListener(
+              (source, nativeMediaRef, javaKeyCode) ->
+                   controller.handleKeyEvents(javaKeyCode));
+          // Open a simple JFrame to control the media player through key commands
+          // Be creative and create your own controller in JavaFX
+          Platform.runLater(
+              () -> {
+                controller = new JMediaPlayerControlFrame(mediaPlayer);
+              });
         });
 
-    // Handle Window Key events triggered from SDL window
-    mediaPlayer.addSdlKeyEventListener(
-            (source, nativeMediaRef, javaKeyCode) ->
-                    controller.handleKeyEvents(javaKeyCode));
+    mediaPlayerTask.setOnFailed(event -> Platform.exit());
+
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    executorService.execute(mediaPlayerTask);
+    executorService.shutdown();
   }
 
   public static void main(String[] args) {
     Application.launch(args);
+  }
+}
+
+class MediaPlayerTask extends Task<MediaPlayer> {
+  private URI mediaPath;
+
+  MediaPlayerTask(URI mediaPath) {
+    this.mediaPath = mediaPath;
+  }
+
+  @Override
+  protected MediaPlayer call() throws Exception {
+    MediaPlayer mediaPlayer = new FfmpegSdlMediaPlayer(mediaPath);
+    mediaPlayer.init();
+    return mediaPlayer;
   }
 }
